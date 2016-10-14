9ba8cc69c11e9c57fa9bc77e026bcaa2561bcfde
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2016-10-13 23:23:12 -0700

    Link to article instead of comment in recent-comment sidebar
    Fixes tog22/eaforum#75.

--- r2/r2/lib/utils/http_utils.py
+++ r2/r2/lib/utils/http_utils.py
@@ -2,6 +2,7 @@ import pytz
 import urllib
 import urlparse
 from datetime import datetime
+import re
 
 DATE_RFC822 = '%a, %d %b %Y %H:%M:%S %Z'
 DATE_RFC850 = '%A, %d-%b-%y %H:%M:%S %Z'
@@ -34,3 +35,30 @@ def set_query_parameter(url, param_name, param_value):
     new_query_string = urllib.urlencode(query_params, doseq=True)
 
     return urlparse.urlunsplit((scheme, netloc, path, new_query_string, fragment))
+
+def generate_comment_url(url, fullname):
+    """
+    If URL is for a comment, replace the path to the comment permalink
+    with the path to the post, and add a fragment identifier to scroll
+    to the comment.
+
+    It would be better to get the post URL in a more-direct manner and
+    use a helper to append the hash for the comment.
+    """
+    regex_string = """
+        ^
+        /ea
+        /(?P<post_id>[^/]+)
+        /(?P<post_slug>[^/]+)
+        /(?P<comment_id>[^/]+)
+        /?
+        $
+    """
+    regex = re.compile(regex_string, re.VERBOSE)
+    match = regex.match(url)
+    if match and match.group('comment_id'):
+        groupdict = match.groupdict()
+        groupdict['fullname'] = fullname
+        return '/ea/{post_id}/{post_slug}/#thingrow_{fullname}'.format(**groupdict)
+    else:
+        return url
